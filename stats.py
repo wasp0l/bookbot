ed396702006266d3972e83de659e38b77e43466f
wasp0l <unecorrigan@gmail.com>
2025-08-26 01:13:30 +0100

    updated stats.py as per the count characters lesson

--- stats.py
+++ stats.py
@@ -7,10 +7,21 @@ def count_words(book_text):
     word_list = book_text.split()
     i = len(word_list)
     return i
-    
+
+def count_characters(book_text):
+    character_count = {}
+    for character in book_text.lower():
+        if character in character_count:
+            character_count[character] += 1
+        else:
+            character_count[character] = 1
+    return character_count
+
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_count = count_words(book_text)
+    character_count = count_characters(book_text.lower())
     print(f"{word_count} words found in the document")
+    print (f"{character_count}")
 
 main()
