783edae91ca350a282c69ab5af76c74f946df363
wasp0l <unecorrigan@gmail.com>
2025-08-27 23:50:44 +0100

    updated main.py so path is no longer hard coded

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # python
 from stats import get_book_text, count_words, count_characters, sort_dictionaries
+import sys
 
-BOOK_PATH = "books/frankenstein.txt"
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    BOOK_PATH = sys.argv[1]
 
 def main():
     book_text = get_book_text(BOOK_PATH)
@@ -9,14 +14,14 @@ def main():
     char_counts = count_characters(book_text)  # count_characters lowercases internally
     sorted_chars = sort_dictionaries(char_counts)
 
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {BOOK_PATH}...")
-    print("----------- Word Count ----------")
-    print(f"Found {word_count} total words")
-    print("--------- Character Count -------")
+    #print("============ BOOKBOT ============")
+    #print(f"Analyzing book found at {BOOK_PATH}...")
+    #print("----------- Word Count ----------")
+    #print(f"Found {word_count} total words")
+    #print("--------- Character Count -------")
     for item in sorted_chars:
         print(f"{item['char']}: {item['num']}")
-    print("============= END ===============")
+    #print("============= END ===============")
 
 if __name__ == "__main__":
     main()
